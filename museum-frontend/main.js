1fe21c58d1f04ab6ee1b96bbeb1978308c80abba
Simon JJ Lake <simonjjlake@gmail.com>
2022-02-19 14:17:29 +0000

    Basic three js project

--- museum-frontend/main.js
+++ museum-frontend/main.js
@@ -1,6 +1,25 @@
 import './style.css'
+import * as THREE from "three"
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg")
+})
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x00ffff)
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function init() {
+  onWindowResize();
+  window.addEventListener("resize", onWindowResize);
+  renderer.render(scene, camera);
+  animate();
+}
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera)
+}
+init();

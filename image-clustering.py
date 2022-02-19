ca5fa3bdee2be663f09b6b35b114fe5eb35e3f3c
DavidAHazra <david.ogden493@googlemail.com>
2022-02-19 14:45:24 +0000

    dict type

--- image-clustering.py
+++ image-clustering.py
@@ -12,7 +12,7 @@ def get_image_data(file_path):
 
 if __name__ == '__main__':
     image_data = get_image_data(os.path.join(os.getcwd(), "data", "data_description.xml"))
-    num_of_category = defaultdict(set)
+    num_of_category = defaultdict(int)
 
     for image in image_data:
         if image["categories"] is not None:

1c1d8193bf4fa6c36633ed0e1ab5a5275f888c32
DavidAHazra <david.ogden493@googlemail.com>
2022-02-20 09:45:40 +0000

    gonrgjnregnrekgn

---
+++ convert-xml.py
@@ -0,0 +1,17 @@
+import os
+import xmltodict
+from collections import defaultdict
+import json
+
+
+def get_image_data(file_path):
+    with open(file_path, 'r') as file_handle:
+        dict_data = xmltodict.parse(file_handle.read())["root"]
+
+    return dict_data["pppmediaitem"]
+
+
+if __name__ == '__main__':
+    image_data = get_image_data(os.path.join(os.getcwd(), "data", "data_description.xml"))
+    with open(os.path.join(os.getcwd(), "data", "data_desc.json"), 'w') as wr:
+        wr.write(json.dumps(image_data))
